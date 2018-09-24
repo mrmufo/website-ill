71388165a076b6104b8f29c2c4d0a1dc2def8579
mrmufo <rykala.wojciech@gmail.com>
2018-09-24 21:27:34 +0100

    Added some jQuery - changing logo colors

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,30 @@
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {
-        indicators: true
+    indicators: true
     });
-  });
+});
+$(document).ready(function() {
+    var mmm = 5;
+    var intervalID;
+    function changeColor() {
+        intervalID = window.setInterval(getValues, 100);
+    };
+    
+    function getValues(){
+        //console.log($('.sliding-insane'));
+        var matrix = $('.sliding-insane').css('transform').replace(/[^0-9\-.,]/g, '').split(',');
+        var x = matrix[4];
+        console.log(x);
+        if(x < 0) {
+            $("#logo-I").css("color", "#ffab00");
+        }
+    };
+    
+    function stopTextColor() {
+        clearInterval(intervalID);
+    };
+    
+    
+    changeColor();
+})
