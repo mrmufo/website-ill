870bab0d55d03921bb7e94d8c0a4c7c022a3b9fd
mrmufo <rykala.wojciech@gmail.com>
2018-10-05 22:03:07 +0100

    Added logo highlight, changed header text animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,12 +13,23 @@ $(document).ready(function() {
     function getValues(){
         //console.log($('.sliding-insane'));
         var matrix = $('.sliding-insane').css('transform').replace(/[^0-9\-.,]/g, '').split(',');
-        var x = matrix[4];
+        var x = matrix[4]/window.screen.availWidth;
         console.log(x);
-        if(x < 0) {
+        if(x > -0.475 && x < -0.12) {
             $("#logo-I").css("color", "#ffab00");
-            stopTextColor();
+        }else if(x > -0.775 && x < -0.475) {
+            $("#logo-N1").css("color", "#ffab00");
+        }else if(x > -1.125 && x < -0.775) {
+            $("#logo-S").css("color", "#ffab00");
+        }else if(x > -1.375 && x < -1.125) {
+            $("#logo-A").css("color", "#ffab00");
+        }else if(x > -1.735 && x < -1.375) {
+            $("#logo-N2").css("color", "#ffab00");
+        }else if(x < -1.735) {
+            $("#logo-E").css("color", "#ffab00");
+            clearInterval(intervalID);
         }
+        
     };
     
     function stopTextColor() {
