4a9379d92be43f6bb454a5d9364e029b79dea68a
mrmufo <rykala.wojciech@gmail.com>
2018-10-10 17:43:56 +0100

    Added jquery keyframes plugin, animated logo

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 $(document).ready(function() {
+    $.keyframe.debug = true;
     var intervalID;
     var letterWidth;
     var insane = document.getElementById("sliding-insane");
@@ -21,17 +22,17 @@ $(document).ready(function() {
         var matrix = $('#sliding-insane').css('transform').replace(/[^0-9\-.,]/g, '').split(',');
         var x = matrix[4];
         if(-2*letterWidth < x && x <= -letterWidth) {
-            $("#logo-I").css("color", "#ffab00");
+            $("#logo-I").css({"color": "#ffab00", "opacity": "1"});
         }else if(-3*letterWidth < x && x <= -2*letterWidth) {
-            $("#logo-N1").css("color", "#ffab00");
+            $("#logo-N1").css({"color": "#ffab00", "opacity": "1"});
         }else if(-4*letterWidth < x && x <= -3*letterWidth) {
-            $("#logo-S").css("color", "#ffab00");
+            $("#logo-S").css({"color": "#ffab00", "opacity": "1"});
         }else if(-5*letterWidth < x && x <= -4*letterWidth) {
-            $("#logo-A").css("color", "#ffab00");
+            $("#logo-A").css({"color": "#ffab00", "opacity": "1"});
         }else if(-6*letterWidth < x && x <= -5*letterWidth) {
-            $("#logo-N2").css("color", "#ffab00");
+            $("#logo-N2").css({"color": "#ffab00", "opacity": "1"});
         }else if(x <= -6*letterWidth) {
-            $("#logo-E").css("color", "#ffab00");
+            $("#logo-E").css({"color": "#ffab00", "opacity": "1"});
             stopTextColor();
         }
     };
@@ -40,6 +41,7 @@ $(document).ready(function() {
         clearInterval(intervalID);
     };
 
+    
     
     letterWidth = measureLetterWidth(insane, 6);
     changeColor();
